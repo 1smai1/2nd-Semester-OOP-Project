efd9019e162c8d4f7f6d5dcd70b0c8974e3126dd
1smai1 <idkmaybeismail@gmail.com>
2024-05-12 18:40:57 +0500

    Add files via upload

---
+++ StartTimer.h
@@ -0,0 +1,24 @@
+// Project: Gym progression tracker
+// Coders:  Ismail Khan Mohmand & Rayan Hussain
+// Purpose: To track your gym progress and log it
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+#include <chrono> // For timer
+
+using namespace std;
+using namespace std::chrono;
+
+void startTimer(int timer_duration)
+{
+    auto start_time = system_clock::now();
+    auto end_time = start_time + seconds(timer_duration);
+    
+    while (system_clock::now() < end_time)
+    {
+        // wait
+    }
+}
